5b109215709bc2e34599614dc7a41551c740b698
Skyline-23 <bs2740@naver.com>
2021-07-05 14:58:08 +0900

    [UPDATE] LoginModel 추가

--- stack-app/Controller/loginVC.swift
+++ stack-app/Controller/loginVC.swift
@@ -22,7 +22,7 @@ class loginVC: UIViewController, UITextFieldDelegate {
     
     lazy var reRequset = UIAlertController(title: nil, message: serverMessage, preferredStyle: .alert).then {
         $0.addAction(UIAlertAction(title: "취소", style: .destructive, handler: nil))
-        $0.addAction(UIAlertAction(title: "재시도", style: .default) {_ in self.submit() })
+//        $0.addAction(UIAlertAction(title: "재시도", style: .default) {_ in self.submit() })
     }
     
     @IBOutlet weak var backImageView: UIImageView!
@@ -64,7 +64,7 @@ class loginVC: UIViewController, UITextFieldDelegate {
         }
         else {
             textField.resignFirstResponder()
-            submit()
+//            submit()
         }
         return true
     }
@@ -78,56 +78,12 @@ class loginVC: UIViewController, UITextFieldDelegate {
         }
     }
     
-    private func submit() {
-        // 터치 입력 비활성화
-        self.view.isUserInteractionEnabled = false
-        
-        let param: Parameters = [
-            "id": "\(Idtextfield.text ?? "")",
-            "pw": "\(PWtextfield.text ?? "")"
-        ]
-        
-        Networking.post(uri: "/auth/login", param: param, header: nil) { error, data in
-            // 유저 터치 활성화
-            self.view.isUserInteractionEnabled = true
-            
-            // 정상
-            if (error == nil) {
-                let decoder = JSONDecoder()
-                let jsonData = try? decoder.decode(LoginResponse.self, from: data!)
-                switch jsonData?.code {
-                case 200:
-                    Token.shared.token = jsonData?.data?.token
-                    if let menuScreen = self.storyboard?.instantiateViewController(withIdentifier: "Navigation") {
-                        menuScreen.modalPresentationStyle = .fullScreen
-                        menuScreen.modalTransitionStyle = .crossDissolve
-                        self.dismiss(animated: false) {
-                            self.present(menuScreen, animated: true, completion: nil)
-                        }
-                    }
-                case 401, 404:
-                    self.serverMessage = jsonData!.message
-                    self.present(self.alert, animated: true, completion: nil)
-                default:
-                    self.serverMessage = "알 수 없는 오류입니다."
-                    self.present(self.alert, animated: true, completion: nil)
-                }
-            }
-            else {
-                // error is not null
-                print(error!)
-                self.serverMessage = "네트워크를 다시 확인해주세요"
-                self.present(self.reRequset, animated: true, completion: nil)
-            }
-        }
-    }
-    
     //터치가 시작됬을 경우, 키보드를 닫음
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
     @IBAction func submitTouch(_ sender: Any) {
-        submit()
+//        submit()
     }
 }
--- stack-app/Model/Login.swift
+++ stack-app/Model/Login.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import Alamofire
+import RxSwift
 
 struct LoginResponse: Decodable {
     var code: Int
@@ -15,4 +17,59 @@ struct LoginResponse: Decodable {
 
 struct LoginData: Decodable {
     var token: String
+}
+
+struct User: Codable {
+    let name: String
+}
+
+enum LoginError: Error {
+    case defaultError
+    case error(code: Int)
+    
+    var msg: String {
+        switch self {
+        case .defaultError:
+            return "ERROR"
+        case .error(let code):
+            return "\(code) Error"
+        }
+    }
+}
+
+struct LoginModel {
+    
+    func requestLogin(id: String, pw: String) -> Observable<Result<LoginResponse, LoginError>> {
+        return Observable.create { (observer) -> Disposable in
+            
+            // 파라미터
+            let param: Parameters = [
+                "id": id,
+                "pw": pw
+            ]
+            
+            Networking.post(uri: "/auth/login", param: param, header: nil) { error, data in
+                // 정상
+                if (error == nil) {
+                    let decoder = JSONDecoder()
+                    let jsonData = try? decoder.decode(LoginResponse.self, from: data!)
+                    switch jsonData?.code {
+                    case 200:
+                        Token.shared.token = jsonData?.data?.token
+                        observer.onNext(.success(jsonData!))
+                    case 401, 404:
+                        observer.onNext(.failure(.error(code: jsonData!.code)))
+                    default:
+                        observer.onNext(.failure(.defaultError))
+                    }
+                } else { // 네트워킹 실패
+                    observer.onNext(.failure(.defaultError))
+                    print(error!)
+                }
+            }
+            observer.onCompleted()
+            return Disposables.create()
+        }
+    }
+    
 }
